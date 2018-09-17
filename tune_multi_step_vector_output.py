47b1bffcaa0aecff4a3d9943b5da043f54c2c605
angusrtaylor <angusrtaylor@gmail.com>
2018-09-17 13:36:07 +0000

    dockerfile and job update

--- tune_multi_step_vector_output.py
+++ tune_multi_step_vector_output.py
@@ -8,6 +8,7 @@ from glob import glob
 import argparse
 parser = argparse.ArgumentParser()
 parser.add_argument("--data_dir", type=str)
+parser.add_argument("--output_dir", type=str)
 parser.add_argument("--T", type=int)
 parser.add_argument("--LATENT_DIM_1", type=int)
 parser.add_argument("-LATENT_DIM_2", type=int)
@@ -42,25 +43,8 @@ y_scaler.fit(train[['load']])
 X_scaler = MinMaxScaler()
 train[['load', 'temp']] = X_scaler.fit_transform(train)
 
-
-# Use the TimeSeriesTensor convenience class to:
-# 1. Shift the values of the time series to create a Pandas dataframe containing all the data for a single training example
-# 2. Discard any samples with missing values
-# 3. Transform this Pandas dataframe into a numpy array of shape (samples, time steps, features) for input into Keras
-# 
-# The class takes the following parameters:
-# 
-# - **dataset**: original time series
-# - **H**: the forecast horizon
-# - **tensor_structure**: a dictionary discribing the tensor structure in the form { 'tensor_name' : (range(max_backward_shift, max_forward_shift), [feature, feature, ...] ) }
-# - **freq**: time series frequency
-# - **drop_incomplete**: (Boolean) whether to drop incomplete samples
-
 tensor_structure = {'X':(range(-T+1, 1), ['load', 'temp'])}
 train_inputs = TimeSeriesTensor(train, 'load', HORIZON, tensor_structure)
-
-
-# Construct validation set (keeping W hours from the training set in order to construct initial features)
 
 look_back_dt = dt.datetime.strptime(valid_start_dt, '%Y-%m-%d %H:%M:%S') - dt.timedelta(hours=T-1)
 valid = energy.copy()[(energy.index >=look_back_dt) & (energy.index < test_start_dt)][['load', 'temp']]
@@ -71,11 +55,6 @@ look_back_dt = dt.datetime.strptime(test_start_dt, '%Y-%m-%d %H:%M:%S') - dt.tim
 test = energy.copy()[test_start_dt:][['load', 'temp']]
 test[['load', 'temp']] = X_scaler.transform(test)
 test_inputs = TimeSeriesTensor(test, 'load', HORIZON, tensor_structure)
-
-
-# ## Implement the RNN
-
-# We will implement a RNN forecasting model with the following structure:
 
 from keras.models import Model, Sequential
 from keras.layers import GRU, Dense
@@ -98,7 +77,7 @@ def get_model(LATENT_DIM_1, LATENT_DIM_2=0):
 
 N_EXPERIMENTS = 1
 BATCH_SIZE = 32
-EPOCHS = 500
+EPOCHS = 100
 LATENT_DIM_1 = args.LATENT_DIM_1
 LATENT_DIM_2 = args.LATENT_DIM_2
 
@@ -107,39 +86,38 @@ print("LATENT_DIM_2: ", LATENT_DIM_2)
 
 
 mapes = np.empty(N_EXPERIMENTS)
-for i in range(N_EXPERIMENTS):
-    
-    # Initialize the model
-    model = get_model(LATENT_DIM_1, LATENT_DIM_2)
-    model.summary()
-    earlystop = EarlyStopping(monitor='val_loss', min_delta=0, patience=5)
-    best_val = ModelCheckpoint('model_{epoch:02d}.h5', save_best_only=True, mode='min', period=1)
-    
-    # Train the model
-    history = model.fit(train_inputs['X'],
-                        train_inputs['target'],
-                        batch_size=BATCH_SIZE,
-                        epochs=EPOCHS,
-                        validation_data=(valid_inputs['X'], valid_inputs['target']),
-                        callbacks=[earlystop, best_val],
-                        verbose=1)
-    
-    # load the model with the smallest MAPE
-    best_epoch = np.argmin(np.array(history.history['val_loss']))+1
-    model.load_weights("model_{:02d}.h5".format(best_epoch))
-    
-    predictions = model.predict(test_inputs['X'])
-    
-    # Compute MAPE for each forecast horizon
-    eval_df = create_evaluation_df(predictions, test_inputs, HORIZON, y_scaler)
-    eval_df['APE'] = (eval_df['prediction'] - eval_df['actual']).abs() / eval_df['actual']
-    
-    # Compute MAPE across all predictions
-    mapes[i] = mape(eval_df['prediction'], eval_df['actual'])
-    print('{0:.4f}'.format(mapes[i]))
-    
-    for f in glob('model_*.h5'):
-        os.remove(f)
 
-result = 'Mean MAPE = {0:.4f} +/- {1:.4f}'.format(np.mean(mapes), np.std(mapes)/np.sqrt(N_EXPERIMENTS))
-print(result)
+# Initialize the model
+model = get_model(LATENT_DIM_1, LATENT_DIM_2)
+model.summary()
+earlystop = EarlyStopping(monitor='val_loss', min_delta=0, patience=5)
+best_val = ModelCheckpoint('model_{epoch:02d}.h5', save_best_only=True, mode='min', period=1)
+
+# Train the model
+history = model.fit(train_inputs['X'],
+                    train_inputs['target'],
+                    batch_size=BATCH_SIZE,
+                    epochs=EPOCHS,
+                    validation_data=(valid_inputs['X'], valid_inputs['target']),
+                    callbacks=[earlystop, best_val],
+                    verbose=1)
+
+# load the model with the smallest MAPE
+best_epoch = np.argmin(np.array(history.history['val_loss']))+1
+model.load_weights("model_{:02d}.h5".format(best_epoch))
+
+predictions = model.predict(test_inputs['X'])
+
+# Compute MAPE for each forecast horizon
+eval_df = create_evaluation_df(predictions, test_inputs, HORIZON, y_scaler)
+eval_df['APE'] = (eval_df['prediction'] - eval_df['actual']).abs() / eval_df['actual']
+
+# Compute MAPE across all predictions
+MAPE = mape(eval_df['prediction'], eval_df['actual'])
+print('{0:.4f}'.format(MAPE))
+
+for f in glob('model_*.h5'):
+    os.remove(f)
+
+with open(os.path.join(args.output_dir, "Output.txt"), "w") as file:
+    file.write(MAPE)
