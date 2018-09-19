0cb2d5e712ff7dcf92ac674dbc05bda4c95aeeab
Dmitry Pechyoni <dmpechyo@microsoft.com>
2018-09-19 22:01:15 +0000

    generalize common files to be used with batch ai and outside of jupyter

--- common/utils.py
+++ common/utils.py
@@ -3,10 +3,9 @@ import pandas as pd
 import os
 from collections import UserDict
 
-def load_data():
+def load_data(data_dir):
     """Load the GEFCom 2014 energy load data"""
 
-    data_dir = 'data/'
     energy = pd.read_csv(os.path.join(data_dir, 'energy.csv'), parse_dates=['timestamp'])
 
     # Reindex the dataframe such that the dataframe has a record for every time point
